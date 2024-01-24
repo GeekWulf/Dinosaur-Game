ace233b609ed9872bec46c5aadb07313098db17f
GeekWulf <thailand49y@gmail.com>
2024-01-24 22:31:56 +0700

    code cleaned-1

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
-const dino = document.getElementById("dino");
-const cactus = document.getElementById("cactus");
-const startScreen = document.getElementById("startScreen");
-const screen = document.getElementById("screen");
-var startButton = document.getElementById("startScreen");
-var scoreBoard = document.getElementById("score");
-var bestScore = document.getElementById("hi-score");
-var score = 0;
-var hiScore = 0;
+const dino = document.getElementById("dino"),
+    cactus = document.getElementById("cactus"),
+    startScreen = document.getElementById("startScreen"),
+    screen = document.getElementById("screen");
+
+var startButton = document.getElementById("startScreen"),
+    scoreBoard = document.getElementById("score"),
+    bestScore = document.getElementById("hi-score"),
+    score = 0,
+    hiScore = 0;
+
 let isCollusion = 0;
 
 function jump() {
+    
     if (dino.classList != "jump"){
         dino.classList.add("jump");
         setTimeout(function(){
@@ -17,6 +20,40 @@ function jump() {
         }, 400); 
     }    
 }
+
+
+function renderScore() {
+    score += 1;
+    scoreBoard.innerText = score;
+}
+
+startButton.addEventListener("click", () => {
+    
+    if (isCollusion == 1) {
+        cactus.classList.remove("cactusMove");
+    }
+    
+    cactus.style.animationPlayState = "running";
+    console.log("high score: " + hiScore);
+    startButton.childNodes[0].innerHTML = "";
+    dino.style.backgroundImage = "url(img/t-rex.png)";
+    
+    let isAlive = setInterval(function (){
+        console.log(score);
+        document.addEventListener("keydown", function(event) {
+            jump();
+        });
+        cactus.classList.add("cactusMove");
+        renderScore();   
+        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
+        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
+        console.log(cactusLeft, dinoTop);
+        if (cactusLeft < 90 && cactusLeft > -30 && dinoTop >= 300){ 
+            gameOver();
+            clearInterval(isAlive);
+        }
+    }, 100);
+})
 
 function gameOver() {
     isCollusion = 1;
@@ -36,38 +73,3 @@ function gameOver() {
     startButton.childNodes[0].appendChild(subText);
     subText.innerHTML = "(click to play again)";
 }
-
-function renderScore() {
-    score += 1;
-    scoreBoard.innerText = score;
-}
-
-startButton.addEventListener("click", () => {
-    if (isCollusion == 1) {
-        cactus.classList.remove("cactusMove");
-    }
-
-    cactus.style.animationPlayState = "running";
-    console.log("high score: " + hiScore);
-    startButton.childNodes[0].innerHTML = "";
-    dino.style.backgroundImage = "url(img/t-rex.png)";
-    
-    let isAlive = setInterval(function (){
-        console.log(score);
-        document.addEventListener("keydown", function(event) {
-            jump();
-        });
-        
-        cactus.classList.add("cactusMove");
-
-        renderScore();   
-
-        let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
-        let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-        if (cactusLeft < 90 && cactusLeft > -30 && dinoTop >= 300){
-            gameOver();
-            clearInterval(isAlive);
-        }
-
-    }, 100);
-})
